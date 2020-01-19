7c8316b4bf4cf19dce6fc04df9ba4fa069982837
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-19 02:42:44 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -38,7 +38,6 @@ def find_greater_pair(array1 = [[4,8], [39,21], [16,60], [345,234]])
   # that are in the inner Arrays
   row = 0
   while row < array1.length
-        greater_number = 0
         array1[row].sort!
         array1[row].shift
         row += 1
